7bd0c37b9a90220d2424957a2be308fc636f7744
Mateus Medeiros <mateusdesousamedeiros@hotmail.com>
2017-01-25 22:47:41 -0200

    Adicionando tema

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+ruby RUBY_VERSION
+
+group :jekyll_plugins do
+   gem "jekyll-feed", "~> 0.6"
+   gem "jekyll", "3.3.1"
+   gem 'jekyll-paginate', '~> 1.1'
+   gem 'sass', '~> 3.4', '>= 3.4.19'
+end
---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// Abrir e Fechar - Menu
+$(".bt-menu").click(function() {
+	$(".menu").toggle();
+
+	if ( $(".menu").is(":visible") ) {
+		$(".bt-menu").attr('aria-expanded', 'true');
+
+	} else {
+		$(".bt-menu").attr('aria-expanded', 'false');
+	};
+});
+
+// Abrir e Fechar - Barra de Pesquisa
+$(".bt-search").click(function() {
+	$("#search-container").show();
+
+	if ( $("#search-container").is(":visible") ) {
+		$(".bt-search").attr('aria-expanded', 'true');
+	}
+});
+
+$(".bt-close").click(function() {
+	$("#search-container").hide();
+
+	if ( $("#search-container").is(":hidden") ) {
+		$(".bt-search").attr('aria-expanded', 'false');
+	}
+});
+
+// Simple Jekyll Search
+SimpleJekyllSearch({
+  searchInput: document.getElementById('search-input'),
+  resultsContainer: document.getElementById('results-container'),
+  json: '/search.json',
+  searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+  noResultsText: 'Sem resultados'
+})
+
+$(window).resize(function(){
+	if(window.innerWidth > 800) {
+		$(".menu").removeAttr("style");
+		$("#search-container").removeAttr("style");
+	}
+});
